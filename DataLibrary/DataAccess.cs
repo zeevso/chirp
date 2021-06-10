558694872d27dc41b33dd560b075923ba5808a0d
zeevso <zeevso@gmail.com>
2021-06-10 21:23:55 +0300

    Add data library

---
+++ DataLibrary/DataAccess.cs
@@ -0,0 +1,44 @@
+﻿// 03.06.21 Ze'ev
+// Handling data access routines. Based on Dapper
+using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class DataAccess : IDataAccess
+    {
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<T>(sql, parameters);
+
+                return rows.ToList();
+            }
+        }
+
+        public async Task<T> LoadDataObject <T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var row = await connection.QueryFirstOrDefaultAsync<T>(sql, parameters);
+
+                return row;
+            }
+        }
+
+        public Task SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                return connection.ExecuteAsync(sql, parameters);
+            }
+        }
+    }
+}
---
+++ DataLibrary/IDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString);
+        Task<T> LoadDataObject<T, U>(string sql, U parameters, string connectionString);
+        Task SaveData<T>(string sql, T parameters, string connectionString);
+    }
+}
